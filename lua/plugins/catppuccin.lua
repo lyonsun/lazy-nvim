4200e27b387706140cb57e56c03187ab98e31cfc
Liang Sun <liang.sun@supermetrics.com>
2024-12-31 00:17:19 +0200

    feat: enable transparent background for catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,7 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
+      transparent_background = true,
       flavour = "mocha", -- latte, frappe, macchiato, mocha
       -- flavour = "auto" -- will respect terminal's background
       background = { -- :h background
